ab1388462453cdd33295bc670756a1333c89a09d
feixue22379 <130360638+feixue22379@users.noreply.github.com>
2023-05-12 12:28:32 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
+export const OWNER = "feixue";
+export const REPO = "feixue";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
