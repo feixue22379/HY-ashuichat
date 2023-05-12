9283ff62469ccbbe5e7fa89ab020e116921b82f2
feixue22379 <130360638+feixue22379@users.noreply.github.com>
2023-05-12 10:06:48 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -32,7 +32,12 @@ export const getServerSideConfig = () => {
       "[Server Config] you are importing a nodejs-only module outside of nodejs",
     );
   }
+  
+  const apiKeys = (process.env.OPEN_AI_KEY ?? '').split(',')
+  const apiKey =apiKeys.at(Math.floor(Math.random() * apiKeys.length)) ?? ''
 
+  return {
+    apiKey,
   return {
     apiKey: process.env.OPENAI_API_KEY,
     code: process.env.CODE,
